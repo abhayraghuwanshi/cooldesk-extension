0e75ec7910828374cecf84d7675d5759ffa14983
Abhay raghuwanshi <raghuwanshi.abhay405@gmail.com>
2025-08-16 20:15:20 +0530

    clean up

--- popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('openDashboard').addEventListener('click', () => {
-    console.log('Dashboard button clicked, sending message to background script.');
-    chrome.runtime.sendMessage({ action: 'syncAndOpenDashboard' });
-});
