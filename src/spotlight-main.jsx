dc0d5e64e3918c23064550db8327a97d5213e7ac
Abhay raghuwanshi <45534131+abhayraghuwanshi@users.noreply.github.com>
2026-05-08 13:17:27 +0530

    new bundle

--- src/spotlight-main.jsx
+++ src/spotlight-main.jsx
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { initChromePolyfill } from './services/chromePolyfill';
